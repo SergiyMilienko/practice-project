65124c5d62063e05c434b43110bb357efa26b53e
SergiyMilienko <106393416+SergiyMilienko@users.noreply.github.com>
2023-10-11 20:45:03 +0300

    hiding widgets

--- js/main.js
+++ js/main.js
@@ -15,3 +15,14 @@ btnShowMoreCards.addEventListener('click', function() {
         card.classList.remove('card-link--hidden');
     })
 })
+
+const widgets = document.querySelectorAll('.widget');
+
+widgets.forEach(function (widget) {
+    widget.addEventListener('click', function (e) {
+        if (e.target.classList.contains('widget__title')) {
+            e.target.classList.toggle('widget__title--active');
+            e.target.nextElementSibling.classList.toggle('widget__body--hidden');
+        }
+    })
+})

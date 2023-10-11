b0efa24a7bd192e98b18f24914663cfb59b6eb4d
SergiyMilienko <106393416+SergiyMilienko@users.noreply.github.com>
2023-10-11 20:28:14 +0300

    button show more

--- js/main.js
+++ js/main.js
@@ -6,3 +6,12 @@ sidebarToggleBtn.onclick = function () {
 	menuIcon.classList.toggle('menu-icon-active');
 	sidebar.classList.toggle('sidebar--mobile-active');
 };
+
+const btnShowMoreCards = document.querySelector('.btn-more');
+const hiddenCards = document.querySelectorAll('.card-link--hidden');
+
+btnShowMoreCards.addEventListener('click', function() {
+    hiddenCards.forEach(function (card) {
+        card.classList.remove('card-link--hidden');
+    })
+})
